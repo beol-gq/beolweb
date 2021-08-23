dc6cfc38fb8673ab2427287af5f69909b0d586af
name <beol.gq@gmail.com>
2021-08-23 21:09:25 +0900

    Daily Update

---
+++ member/member_js.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDqRx5NGq7WqSNviytOnGg-_8QYRLqXYBw",
+    authDomain: "jshsus-6144b.firebaseapp.com",
+    databaseURL: "https://jshsus-6144b.firebaseio.com",
+    projectId: "jshsus-6144b",
+    storageBucket: "jshsus-6144b.appspot.com",
+    messagingSenderId: "709620057314"
+};
+firebase.initializeApp(config);
