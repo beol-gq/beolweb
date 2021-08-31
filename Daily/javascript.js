9e2fc826b47d438d7341f8e8b27b1aa5222b476e
name <beol.gq@gmail.com>
2021-08-31 21:14:15 +0900

    Daily Update

---
+++ Daily/javascript.js
@@ -0,0 +1,22 @@
+function toggleDiv_1() {
+    const div = document.getElementById('photo_1');
+    if(div.style.display === 'none')  {
+      div.style.display = 'inline-block';
+    }else {
+      div.style.display = 'none';
+    }
+} function toggleDiv_2() {
+    const div = document.getElementById('photo_2');
+    if(div.style.display === 'none')  {
+      div.style.display = 'inline-block';
+    }else {
+      div.style.display = 'none';
+    }
+} function toggleDiv_3() {
+    const div = document.getElementById('photo_3');
+    if(div.style.display === 'none')  {
+      div.style.display = 'inline-block';
+    }else {
+      div.style.display = 'none';
+    }
+} 
